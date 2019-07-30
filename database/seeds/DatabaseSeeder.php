d86c44a7847a5611f555e18d4688bf417cefc1ef
Mirko Josimovic <mirkojosimovic1987@gmail.com>
2019-07-30 20:47:34 +0200

    DB seed and Authentication

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'Moderator',
+            'password' => '$2y$12$liQgDlT0TzFMm9/MExaVcu32fyq8PwiIG8f0xjtSwTXmVV2qfuArm', // admin1234
+            'email' => 'moderator@example.com',
+            'role' => 'moderator'
+        ]);
+
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'hrmanager1',
+            'password' => '$2y$12$5BGK6kjMj2l5d5.mq/VqG.LPViOsoB41EbzD24hupZCyd9qwarycW', // hr1234
+            'email' => 'hr1@example.com',
+            'role' => 'hr'
+        ]);
+
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'hrmanager2',
+            'password' => '$2y$12$5BGK6kjMj2l5d5.mq/VqG.LPViOsoB41EbzD24hupZCyd9qwarycW', // hr1234
+            'email' => 'hr2@example.com',
+            'role' => 'hr'
+        ]);
+
+        \Illuminate\Support\Facades\DB::table('users')->insert([
+            'name' => 'hrmanager3',
+            'password' => '$2y$12$5BGK6kjMj2l5d5.mq/VqG.LPViOsoB41EbzD24hupZCyd9qwarycW', // hr1234
+            'email' => 'hr3@example.com',
+            'role' => 'hr'
+        ]);
     }
 }
